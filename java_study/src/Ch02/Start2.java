5a3bc584bc1a6c2b9e285faee8cf953b47aeeb29
yejiCho <goe152@naver.com>
2020-09-13 00:08:36 +0900

    java 객체지향

---
+++ java_study/src/Ch02/Start2.java
@@ -0,0 +1,10 @@
+package Ch02;
+
+public class Start2 {
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+		
+		double d = 20.0;
+	}
+}
---
+++ java_study/src/Ch02/Start3.java
@@ -0,0 +1,20 @@
+package Ch02;
+
+public class Start3 {
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int k = 20;
+		
+		if(i==10) {
+			int m = k+5;
+			k = m;
+		} else {
+			int p = k + 10;
+			k = 10;
+		}
+		
+//		k = m+p;
+	}
+
+}
